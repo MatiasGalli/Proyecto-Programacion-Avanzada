707a8699640e2ab41e596b1faa8cf24dbae4b48a
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-11-09 17:40:13 -0300

    Modificacion de atributos de la tabla 'products' de la base de datos (traduccion)

--- Stack Overflow House/src/logica/Products.java
+++ Stack Overflow House/src/logica/Products.java
@@ -1,66 +1,59 @@
 package logica;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class Products {
-	
+
 	public void getProducts(SQL_Manager connection) {
-		
+
 		String sql;
 		Statement st;
 		ResultSet rs;
-		sql = "Select * from productos";
-		
+		sql = "Select * from product";
+
 		try {
 			st = connection.getConnection().createStatement();
 			rs = st.executeQuery(sql);
 			displayProducts(rs);
-					
-		}catch(SQLException ex) {
+
+		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
-		
+
 	}
-	
+
 	public void displayProducts(ResultSet rs) throws SQLException {
-		while(rs.next()) {
-			System.out.println(rs.getString("id_producto")+ "\t"
-					+ rs.getString("nombre_producto") + "\t"
-					+ rs.getString("descripcion") + "\t"
-					+ rs.getString ("precio") + "\t"
-					+ rs.getString("stock") + "\t"
-					+ rs.getString("id_categoria"));
-			
+		while (rs.next()) {
+			System.out.println(rs.getString("id") + "\t" + rs.getString("name") + "\t" + rs.getString("description")
+					+ "\t" + rs.getString("price") + "\t" + rs.getString("stock") + "\t" + rs.getString("category_id"));
+
 		}
 	}
 
-
-	
-	public void insertProduct(SQL_Manager connection, String id_producto, String nombre_producto, String descripcion, int precio, int stock, String id_categoria) throws SQLException {
+	public void insertProduct(SQL_Manager connection, String id, String name, String description, int price, int stock,
+			String category_id) throws SQLException {
 		try {
 			String sql;
-		
-			sql = "INSERT INTO producto (id_producto,nombre_producto,descripcion,precio,stock,id_categoria) values (?,?,?,?,?,?)";
-		
+
+			sql = "INSERT INTO product (id,name,description,price,stock,category_id) values (?,?,?,?,?,?)";
+
 			PreparedStatement st = connection.getConnection().prepareStatement(sql);
-		
-			st.setString(1,id_producto);
-			st.setString(2, nombre_producto);
-			st.setString(3, descripcion);
-			st.setInt(4, precio);
+
+			st.setString(1, id);
+			st.setString(2, name);
+			st.setString(3, description);
+			st.setInt(4, price);
 			st.setInt(4, stock);
-			st.setString(6, id_categoria);
-		
+			st.setString(6, category_id);
+
 			st.executeUpdate();
-		}catch (SQLException ex) {
+		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
-		
+
 	}
 
 }

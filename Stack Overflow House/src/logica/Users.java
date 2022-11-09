237be162fae134f7d79ad625247a074e7bae4aa1
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-11-09 17:33:22 -0300

    Modificacion de atributos de las tablas de la base de datos (traduccion)

--- Stack Overflow House/src/logica/Users.java
+++ Stack Overflow House/src/logica/Users.java
@@ -1,74 +1,55 @@
 package logica;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-import interfaz.LoginSOH;
-
-public class Users {	
-	public  void getUsers(SQL_Manager connection) {
+public class Users {
+	public void getUsers(SQL_Manager connection) {
 		String sql;
 		Statement st;
 		ResultSet rs;
-		sql = "Select rut,nombre_completo from usuario order by rut";
-		
+		sql = "Select rut,fullname from usuario order by rut";
+
 		try {
 			st = connection.getConnection().createStatement();
 			rs = st.executeQuery(sql);
 			displayUsers(rs);
-			
-		}catch(SQLException ex) {
+
+		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
-		}
-		
-		
-		
-	}
-	
-	
-	public  void displayUsers(ResultSet rs) throws SQLException{
-		while(rs.next()) {
-			System.out.println(rs.getString("rut") + "\t"
-					+ rs.getString("nombre_completo"));
 		}
 	}
 
-	public void insertUser(SQL_Manager connection,String rut, String nombre_completo,String nombre_usuario,String direccion,String correo,String contraseña) throws SQLException {
+	public void displayUsers(ResultSet rs) throws SQLException {
+		while (rs.next()) {
+			System.out.println(rs.getString("rut") + "\t" + rs.getString("fullname"));
+		}
+	}
+
+	public void insertUser(SQL_Manager connection, String rut, String fullname, String username, String address,
+			String email, String password) throws SQLException {
 		try {
-			String sql = "insert into usuario(rut,nombre_completo,nombre_usuario,direccion,correo,administrador,contraseña) values (?,?,?,?,?,?,?)";
-		 
+			String sql = "insert into users(rut,fullname,username,address,email,admin,password) values (?,?,?,?,?,?,?)";
 			PreparedStatement st = connection.getConnection().prepareStatement(sql);
-		
 			st.setString(1, rut);
-			st.setString(2,nombre_completo);
-			st.setString(3,nombre_usuario);
-			st.setString(4,direccion);
-			st.setString(5,correo);
-			st.setBoolean(6,false);
-			st.setString(7,contraseña);
+			st.setString(2, fullname);
+			st.setString(3, username);
+			st.setString(4, address);
+			st.setString(5, email);
+			st.setBoolean(6, false);
+			st.setString(7, password);
 			st.executeUpdate();
-		}catch(SQLException ex){
+		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
-	
-	}	
-		
-	public void deleteUser(SQL_Manager connection,String rut) throws SQLException {
-		 String sql = "delete from usuario where rut = ?";
-		 PreparedStatement st =connection.getConnection().prepareStatement(sql);
-		 
-		 st.setString(1, rut);
-		 st.executeUpdate();
-		 
-	}
-	
-		
-		
 	}
 
-
-
+	public void deleteUser(SQL_Manager connection, String rut) throws SQLException {
+		String sql = "delete from users where rut = ?";
+		PreparedStatement st = connection.getConnection().prepareStatement(sql);
+		st.setString(1, rut);
+		st.executeUpdate();
+	}
+}

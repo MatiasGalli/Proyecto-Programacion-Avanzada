a72ba7ca0b4b7664e96e0207b459bbaf56b1a4bd
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-11-10 21:35:10 -0300

    Nueva ventana para Editar Productos (Sólo creación, falta configuración)

---
+++ Stack Overflow House/src/interfaz/AdminMenuProductsEdit.java
@@ -0,0 +1,42 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class AdminMenuProductsEdit extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminMenuProductsEdit frame = new AdminMenuProductsEdit();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminMenuProductsEdit() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}

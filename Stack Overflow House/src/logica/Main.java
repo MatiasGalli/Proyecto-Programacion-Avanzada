f95607b665a66b1a20703a5f9070c023da2ac52c
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-11-11 21:48:24 -0300

    Actualización del main para que la ventana de login se cree en el centro de la pantalla

--- Stack Overflow House/src/logica/Main.java
+++ Stack Overflow House/src/logica/Main.java
@@ -5,14 +5,12 @@ import java.sql.SQLException;
 
 import interfaz.Login;
 
-//STACK OVERFLOW HOUSE
-
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 		SQL_Manager connection = new SQL_Manager();
 		Login v1 = new Login(connection);
+		v1.setLocationRelativeTo(null);
 		v1.setVisible(true);
 	}
-
 }

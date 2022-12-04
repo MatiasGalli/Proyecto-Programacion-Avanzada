9bfe7213ae9a35b9c246759d2f66264849434e65
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-12-04 19:55:28 -0300

    Ventana de banear usuarios completada (Nuevas funciones: Buscar usuarios por término, Actualizar tabla al bloquear/desbloquear usuario)

--- Stack Overflow House/src/interfaz/AdminMenuUsersBan.java
+++ Stack Overflow House/src/interfaz/AdminMenuUsersBan.java
@@ -131,15 +131,56 @@ public class AdminMenuUsersBan extends JFrame {
 		lbl_SOHlogo.setIcon(new ImageIcon(AdminMenuUsersBan.class.getResource("/assets/SOH_logoMin.png")));
 		lbl_SOHlogo.setBounds(10, 567, 36, 26);
 		contentPane.add(lbl_SOHlogo);
+		
+		rdbtn_asc = new JRadioButton("Ascendente");
+		rdbtn_asc.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		rdbtn_asc.setSelected(true);
+		rdbtn_asc.setBounds(510, 512, 140, 21);
+		rdbtn_asc.setBackground(null);
+		contentPane.add(rdbtn_asc);
+
+		rdbtn_desc = new JRadioButton("Descendente");
+		rdbtn_desc.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		rdbtn_desc.setBounds(652, 513, 150, 21);
+		rdbtn_desc.setBackground(null);
+		contentPane.add(rdbtn_desc);
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtn_asc);
+		group.add(rdbtn_desc);
 
 		btn_ban = new JButton("BLOQUEAR");
 		btn_ban.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String rut = (String) table_userInfo.getValueAt(table_userInfo.getSelectedRow(), 0);
-				banUser(connection,rut,true);
-				JFrame jFrame = new JFrame();
-				jFrame.setAlwaysOnTop(true);
-				JOptionPane.showMessageDialog(jFrame,"El usuario ha sido bloqueado exitosamente.");
+				String order = (String) comboBox_categoryOrder.getSelectedItem();
+				if (order.equals("RUT")) {
+					order = "rut";
+				}else if (order.equals("Nombre de usuario")) {
+					order = "username";
+				}else if (order.equals("Nombre Completo")) {
+					order = "fullname";
+				}else if (order.equals("Correo Electrónico")) {
+					order = "email";
+				}else if (order.equals("Bloqueado")) {
+					order = "banned";
+				}
+				group.getSelection().getSelectedObjects();
+				String asc = null;
+				Object[] ascbtn = rdbtn_asc.getSelectedObjects();
+				if (ascbtn == null) {
+					asc = "DESC";
+				}else {
+					asc = "ASC";
+				}
+				int row = table_userInfo.getSelectedRow();
+				if (row != -1) {
+					String rut = (String) table_userInfo.getValueAt(row, 0);
+					banUser(connection,rut,true,order,asc);
+				}else {
+					JFrame jFrame = new JFrame();
+					jFrame.setAlwaysOnTop(true);
+					JOptionPane.showMessageDialog(jFrame,"Selecciona un usuario.");
+				}
+				
 			}
 		});
 		btn_ban.setBackground(Color.WHITE);
@@ -150,11 +191,35 @@ public class AdminMenuUsersBan extends JFrame {
 		btn_unban = new JButton("DESBLOQUEAR");
 		btn_unban.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String rut = (String) table_userInfo.getValueAt(table_userInfo.getSelectedRow(), 0);
-				banUser(connection,rut,false);
-				JFrame jFrame = new JFrame();
-				jFrame.setAlwaysOnTop(true);
-				JOptionPane.showMessageDialog(jFrame,"El usuario ha sido desbloqueado exitosamente.");
+				String order = (String) comboBox_categoryOrder.getSelectedItem();
+				if (order.equals("RUT")) {
+					order = "rut";
+				}else if (order.equals("Nombre de usuario")) {
+					order = "username";
+				}else if (order.equals("Nombre Completo")) {
+					order = "fullname";
+				}else if (order.equals("Correo Electrónico")) {
+					order = "email";
+				}else if (order.equals("Bloqueado")) {
+					order = "banned";
+				}
+				group.getSelection().getSelectedObjects();
+				String asc = null;
+				Object[] ascbtn = rdbtn_asc.getSelectedObjects();
+				if (ascbtn == null) {
+					asc = "DESC";
+				}else {
+					asc = "ASC";
+				}
+				int row = table_userInfo.getSelectedRow();
+				if (row != -1) {
+					String rut = (String) table_userInfo.getValueAt(row, 0);
+					banUser(connection,rut,false,order,asc);
+				}else {
+					JFrame jFrame = new JFrame();
+					jFrame.setAlwaysOnTop(true);
+					JOptionPane.showMessageDialog(jFrame,"Selecciona un usuario.");
+				}
 			}
 		});
 		btn_unban.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -174,29 +239,23 @@ public class AdminMenuUsersBan extends JFrame {
 		comboBox_categoryOrder.setBounds(600, 481, 230, 26);
 		contentPane.add(comboBox_categoryOrder);
 
-		rdbtn_asc = new JRadioButton("Ascendente");
-		rdbtn_asc.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		rdbtn_asc.setSelected(true);
-		rdbtn_asc.setBounds(510, 512, 140, 21);
-		rdbtn_asc.setBackground(null);
-		contentPane.add(rdbtn_asc);
-
-		rdbtn_desc = new JRadioButton("Descendente");
-		rdbtn_desc.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		rdbtn_desc.setBounds(652, 513, 150, 21);
-		rdbtn_desc.setBackground(null);
-		contentPane.add(rdbtn_desc);
-		ButtonGroup group = new ButtonGroup();
-		group.add(rdbtn_asc);
-		group.add(rdbtn_desc);
+		
 
 		btn_search = new JButton("BUSCAR");
 		btn_search.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				//Here the table does not only updates itself, but it deletes the previous one.
-				
-				table_userInfo = updateTable(connection,"fullname","asc");
+				String categorySearch = (String) comboBox_categorySearch.getSelectedItem();
+				if (categorySearch.equals("RUT")) {
+					categorySearch = "rut";
+				}else if (categorySearch.equals("Nombre de usuario")) {
+					categorySearch = "username";
+				}else if (categorySearch.equals("Nombre Completo")) {
+					categorySearch = "fullname";
+				}else if (categorySearch.equals("Correo Electrónico")) {
+					categorySearch = "email";
+				}
+				String search = textField_search.getText();
+				table_userInfo = search(connection,categorySearch,search);
 				scrollPane_userInfo = showUsers(table_userInfo);
 				remove(contentPane.getComponentAt(60, 120));
 				contentPane.add(scrollPane_userInfo);
@@ -243,42 +302,7 @@ public class AdminMenuUsersBan extends JFrame {
 		btn_order.setBounds(580, 550, 132, 26);
 		contentPane.add(btn_order);
 	}
-	
-	/*
-	public Object[][] getUsers(SQL_Manager connection, Object[][] values) {
 
-		Object[][] list = values;
-		String sql, ban;
-		Statement st;
-		ResultSet rs;
-		sql = "Select * from users order by fullname";
-
-		try {
-			st = connection.getConnection().createStatement();
-			rs = st.executeQuery(sql);
-			int cant = 0;
-			while (rs.next()) {
-				if (!rs.getBoolean("admin")) {
-					list[cant][0] = rs.getString("rut");
-					list[cant][1] = rs.getString("username");
-					list[cant][2] = rs.getString("fullname");
-					list[cant][3] = rs.getString("email");
-					if (rs.getBoolean("banned"))
-						ban = "Si";
-					else
-						ban = "No";
-					list[cant][4] = ban;
-					cant++;
-				}
-			}
-
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return list;
-	}
-	
-	*/
 	
 	public int countUsers(SQL_Manager connection) throws SQLException {
 
@@ -295,9 +319,22 @@ public class AdminMenuUsersBan extends JFrame {
 		return 0;
 	}
 
-	public void banUser(SQL_Manager connection, String rut, boolean ban) {
-		String sql = "update users set banned = ? where rut = ?";
+	public void banUser(SQL_Manager connection, String rut, boolean ban, String order, String asc) {
+		String sql;
 		PreparedStatement st;
+		boolean ban_user = false;
+		sql = "select banned from users where rut = ?";
+		try {
+			st = connection.getConnection().prepareStatement(sql);
+			st.setString(1, rut);
+			ResultSet rs = st.executeQuery();
+			if (rs.next()) {
+				ban_user = rs.getBoolean("banned");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		sql = "update users set banned = ? where rut = ?";
 		try {
 			st = connection.getConnection().prepareStatement(sql);
 			st.setBoolean(1, ban);
@@ -306,69 +343,32 @@ public class AdminMenuUsersBan extends JFrame {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	/*
-	 * Código Legacy. No usar. Por ahora.
-	 * 
-	public JScrollPane showUsers(SQL_Manager connection, JTable table, String category, String asc){
-		try{
-			table.setShowVerticalLines(false);
-			table.setFont(new Font("Tahoma", Font.PLAIN, 16));
-			table.setBorder(null);
-			String sql, ban;
-			PreparedStatement st;
-			ResultSet rs;
-			sql = "Select * from users order by " + category + " " + asc;
-			st = connection.getConnection().prepareStatement(sql);
-			rs = st.executeQuery();
-			String titles[]={"RUT", "Nombre de usuario","Nombre Completo", "Correo Electr\u00F3nico", "Bloqueado"};
-			DefaultTableModel model = new DefaultTableModel(null,titles) {
-				boolean[] columnEditables = new boolean[] {
-						false, false, false, false, false
-					};
-					public boolean isCellEditable(int row, int column) {
-						return columnEditables[column];
-					}
-				};
-			String row[]=new String[6];
-			while(rs.next()){
-				if (!rs.getBoolean("admin")) {
-					row[0]=rs.getString("rut");
-					row[1]=rs.getString("username");
-					row[2]=rs.getString("fullname");
-					row[3]=rs.getString("email");
-					if (rs.getBoolean("banned"))
-						ban = "Si";
-					else
-						ban = "No";
-					row[4]= ban;
-					model.addRow(row);
-				}
+		if (ban == true) {
+			if (ban_user == true) {
+				JFrame jFrame = new JFrame();
+				jFrame.setAlwaysOnTop(true);
+				JOptionPane.showMessageDialog(jFrame,"El usuario ya se encuentra bloqueado.");
+			}else {
+				JFrame jFrame = new JFrame();
+				jFrame.setAlwaysOnTop(true);
+				JOptionPane.showMessageDialog(jFrame,"El usuario ha sido bloqueado exitosamente.");
 			}
-			table.setModel(model);
-			table.getColumnModel().getColumn(0).setPreferredWidth(69);
-			table.getColumnModel().getColumn(1).setResizable(false);
-			table.getColumnModel().getColumn(1).setPreferredWidth(83);
-			table.getColumnModel().getColumn(2).setResizable(false);
-			table.getColumnModel().getColumn(2).setPreferredWidth(213);
-			table.getColumnModel().getColumn(3).setResizable(false);
-			table.getColumnModel().getColumn(3).setPreferredWidth(172);
-			table.getColumnModel().getColumn(4).setPreferredWidth(15);
-			table.setRowHeight(26);
-			table.setBounds(60, 110, 900, 310);
-			JScrollPane scrollPane_userInfo = new JScrollPane(table);
-			scrollPane_userInfo.setLocation(60, 120);
-			scrollPane_userInfo.setSize(900, 330);
-			scrollPane_userInfo.setViewportView(table);
-			return scrollPane_userInfo;
-		}catch(Exception e){
-			JOptionPane.showMessageDialog(null,e);
+		}else{
+			if (ban_user == false) {
+				JFrame jFrame = new JFrame();
+				jFrame.setAlwaysOnTop(true);
+				JOptionPane.showMessageDialog(jFrame,"El usuario ya se encuentra desbloqueado.");
+			}else {
+				JFrame jFrame = new JFrame();
+				jFrame.setAlwaysOnTop(true);
+				JOptionPane.showMessageDialog(jFrame,"El usuario ha sido desbloqueado exitosamente.");
+			}
 		}
-		return null;
+		table_userInfo = updateTable(connection,"fullname","asc");
+		scrollPane_userInfo = showUsers(table_userInfo);
+		remove(contentPane.getComponentAt(60, 120));
+		contentPane.add(scrollPane_userInfo);
 	}
-	
-	*/
 	
 	//Crea un JScrollPane a partir de una tabla.
 	public JScrollPane showUsers (JTable table){
@@ -420,6 +420,68 @@ public class AdminMenuUsersBan extends JFrame {
 					row[4]= ban;
 					model.addRow(row);
 				}
+			}
+			table.setModel(model);
+			table.getColumnModel().getColumn(0).setPreferredWidth(69);
+			table.getColumnModel().getColumn(1).setResizable(false);
+			table.getColumnModel().getColumn(1).setPreferredWidth(83);
+			table.getColumnModel().getColumn(2).setResizable(false);
+			table.getColumnModel().getColumn(2).setPreferredWidth(213);
+			table.getColumnModel().getColumn(3).setResizable(false);
+			table.getColumnModel().getColumn(3).setPreferredWidth(172);
+			table.getColumnModel().getColumn(4).setPreferredWidth(15);
+			table.setRowHeight(26);
+			table.setBounds(60, 110, 900, 310);
+			return (table);
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null,e);
+		}
+		return null;
+	}
+	
+	public JTable search(SQL_Manager connection, String category, String search){
+		try{
+			JTable table = new JTable();
+			table.setShowVerticalLines(false);
+			table.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			table.setBorder(null);
+			String sql, ban;
+			int cont = 0;
+			PreparedStatement st;
+			ResultSet rs;
+			sql = "Select * from users where " + category + " = ?;";
+			st = connection.getConnection().prepareStatement(sql);
+			st.setString(1, search);
+			rs = st.executeQuery();
+			String titles[]={"RUT", "Nombre de usuario","Nombre Completo", "Correo Electr\u00F3nico", "Bloqueado"};
+			DefaultTableModel model = new DefaultTableModel(null,titles) {
+				boolean[] columnEditables = new boolean[] {
+						false, false, false, false, false
+					};
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				};
+			String row[]=new String[6];
+			while(rs.next()){
+				if (!rs.getBoolean("admin")) {
+					row[0]=rs.getString("rut");
+					row[1]=rs.getString("username");
+					row[2]=rs.getString("fullname");
+					row[3]=rs.getString("email");
+					if (rs.getBoolean("banned"))
+						ban = "Si";
+					else
+						ban = "No";
+					row[4]= ban;
+					model.addRow(row);
+					cont++;
+				}
+			}
+			if (cont == 0) {
+				JFrame jFrame = new JFrame();
+				jFrame.setAlwaysOnTop(true);
+				JOptionPane.showMessageDialog(jFrame,"No hay coincidencias en la búsqueda");
 			}
 			table.setModel(model);
 			table.getColumnModel().getColumn(0).setPreferredWidth(69);

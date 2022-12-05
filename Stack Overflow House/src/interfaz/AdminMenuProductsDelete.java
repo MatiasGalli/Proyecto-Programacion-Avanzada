c9da4e9e23476cc157392da43a48cf7cba94baad
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-12-04 21:24:29 -0300

    Ventana de eliminar productos creada y completada (Botón eliminar funcionando, Implementación de una lista que muestra todos los productos)

--- Stack Overflow House/src/interfaz/AdminMenuProductsDelete.java
+++ Stack Overflow House/src/interfaz/AdminMenuProductsDelete.java
@@ -1,15 +1,36 @@
 package interfaz;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
+import logica.SQL_Manager;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Toolkit;
+import java.awt.Font;
+
+@SuppressWarnings("serial")
 public class AdminMenuProductsDelete extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table_products;
+	private JScrollPane scrollPane_products;
 
 	/**
 	 * Launch the application.
@@ -18,7 +39,7 @@ public class AdminMenuProductsDelete extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AdminMenuProductsDelete frame = new AdminMenuProductsDelete();
+					AdminMenuProductsDelete frame = new AdminMenuProductsDelete(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +51,163 @@ public class AdminMenuProductsDelete extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public AdminMenuProductsDelete() {
+	public AdminMenuProductsDelete(SQL_Manager connection) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(AdminMenuProductsDelete.class.getResource("/assets/SOH_logo.png")));
+		setBackground(new Color(255, 250, 205));
+		setTitle("Eliminar Producto");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 960, 540);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 250, 205));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table_products = new JTable();
+		scrollPane_products = new JScrollPane();
+		
+		table_products = updateTable(connection);
+		scrollPane_products = showProducts(table_products);
+		contentPane.add(scrollPane_products);
+		
+		JButton btn_back = new JButton();
+		btn_back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AdminMenu v4 = new AdminMenu(connection);
+				v4.setLocationRelativeTo(null);
+				v4.setVisible(true);
+				dispose();
+			}
+		});
+		btn_back.setIcon(new ImageIcon(AdminMenuProductsDelete.class.getResource("/assets/back.png")));
+		btn_back.setBorder(null);
+		btn_back.setBackground((Color) null);
+		btn_back.setBounds(10, 10, 30, 30);
+		contentPane.add(btn_back);
+		
+		JLabel lbl_SOHlogo = new JLabel("Image");
+		lbl_SOHlogo.setIcon(new ImageIcon(AdminMenuProductsDelete.class.getResource("/assets/SOH_logoMin.png")));
+		lbl_SOHlogo.setBounds(10, 467, 36, 26);
+		contentPane.add(lbl_SOHlogo);
+		
+		JButton btn_delete = new JButton("ELIMINAR");
+		btn_delete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = table_products.getSelectedRow();
+				if (row != -1) {
+					String id = (String) table_products.getValueAt(row, 0);
+					try {
+						deleteProduct(connection,id);
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+				}else {
+					JFrame jFrame = new JFrame();
+					jFrame.setAlwaysOnTop(true);
+					JOptionPane.showMessageDialog(jFrame,"Selecciona un producto.");
+				}
+			}
+		});
+		btn_delete.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btn_delete.setBackground(Color.WHITE);
+		btn_delete.setBounds(400, 420, 138, 26);
+		contentPane.add(btn_delete);
+		
+		JLabel lbl_search = new JLabel("Seleccione un producto de la lista:");
+		lbl_search.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_search.setBounds(60, 35, 359, 26);
+		contentPane.add(lbl_search);
 	}
+	
+	//Crea un JScrollPane a partir de una tabla.
+		public JScrollPane showProducts (JTable table){
+			try{
+				JScrollPane scrollPane_userInfo = new JScrollPane(table);
+				scrollPane_userInfo.setLocation(60, 70);
+				scrollPane_userInfo.setSize(820, 330);
+				scrollPane_userInfo.setViewportView(table);
+				return scrollPane_userInfo;
+			}catch(Exception e){
+				JOptionPane.showMessageDialog(null,e);
+			}
+			return null;
+		}
+		
+		//Crea una tabla nueva dado ciertos parámetros.
+		public JTable updateTable(SQL_Manager connection){
+			try{
+				JTable table = new JTable();
+				table.setShowVerticalLines(false);
+				table.setFont(new Font("Tahoma", Font.PLAIN, 16));
+				table.setBorder(null);
+				String sql, category;
+				PreparedStatement st;
+				ResultSet rs;
+				sql = "Select * from product order by id asc";
+				st = connection.getConnection().prepareStatement(sql);
+				rs = st.executeQuery();
+				String titles[]={"ID", "Nombre del producto","Precio", "Stock", "Categoría"};
+				DefaultTableModel model = new DefaultTableModel(null,titles) {
+					boolean[] columnEditables = new boolean[] {
+							false, false, false, false, false
+						};
+						public boolean isCellEditable(int row, int column) {
+							return columnEditables[column];
+						}
+					};
+				String row[]=new String[6];
+				while(rs.next()){
+					row[0]=rs.getString("id");
+					row[1]=rs.getString("name");
+					row[2]= "$ " + rs.getString("price");
+					row[3]=rs.getString("stock");
+					category = getCategories(connection,rs.getString("category_id"));
+					row[4]= category;
+					model.addRow(row);
+					
+				}
+				table.setModel(model);
+				table.getColumnModel().getColumn(0).setPreferredWidth(20);
+				table.getColumnModel().getColumn(1).setResizable(false);
+				table.getColumnModel().getColumn(1).setPreferredWidth(220);
+				table.getColumnModel().getColumn(2).setResizable(false);
+				table.getColumnModel().getColumn(2).setPreferredWidth(80);
+				table.getColumnModel().getColumn(3).setResizable(false);
+				table.getColumnModel().getColumn(3).setPreferredWidth(40);
+				table.getColumnModel().getColumn(4).setResizable(false);
+				table.getColumnModel().getColumn(4).setPreferredWidth(220);
+				table.setRowHeight(26);
+				table.setBounds(60, 70, 820, 330);
+				return (table);
+			}catch(Exception e){
+				JOptionPane.showMessageDialog(null,e);
+			}
+			return null;
+		}
+		
+		public String getCategories(SQL_Manager connection, String id) throws SQLException {
 
+			String sql = "select name from category where id = ?";
+			PreparedStatement st;
+			st = connection.getConnection().prepareStatement(sql);
+			st.setString(1, id);
+			ResultSet rs = st.executeQuery();
+			rs.next();
+			String name = rs.getString(1);
+			return name;
+		}
+		
+		public void deleteProduct(SQL_Manager connection, String id) throws SQLException {
+			String sql = "delete from product where id = ?";
+			PreparedStatement st = connection.getConnection().prepareStatement(sql);
+			st.setString(1, id);
+			st.executeUpdate();
+			JFrame jFrame = new JFrame();
+			jFrame.setAlwaysOnTop(true);
+			JOptionPane.showMessageDialog(jFrame,"Producto eliminado");
+			table_products = updateTable(connection);
+			scrollPane_products = showProducts(table_products);
+			remove(contentPane.getComponentAt(60, 120));
+			contentPane.add(scrollPane_products);
+		}
 }
